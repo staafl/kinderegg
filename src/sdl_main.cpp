261031652b57bbf51f66068179fd707540182eb2
Velko Nikolov <velko.nikolov@tick42.com>
2020-01-31 22:46:19 +0200

    src/sdl_main.cpp
    [master]

--- src/sdl_main.cpp
+++ src/sdl_main.cpp
@@ -327,15 +327,20 @@ int main(void)
         std::vector<char> buffer(size);
         if (file.read(buffer.data(), size))
         {
+            std::cout<<"Read texture "<<texname<<std::eol;
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
             glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
-            glTexImage2D(GL_TEXTURE_2D,
-                0, mode,
-                w, h,
-                0, mode,
+            glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                mode,
+                w,
+                h,
+                0,
+                mode,
                 GL_UNSIGNED_BYTE,
                 &buffer[0]);
             glGenerateMipmap(GL_TEXTURE_2D);
